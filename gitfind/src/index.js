81124a3df19efd95ae151bcc27fd94406f8fb755
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-30 18:08:56 -0300

    new project

--- gitfind/src/index.js
+++ gitfind/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import App from "./App";
+import App from "./pages/Home";
+import "./style.css"
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
---
+++ gitfind/src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import {Header} from '../../components/Header'
+import background from "../../assets/image-background.svg"
+import './styles.css'
+
+
+function App(){
+    return (
+        <div className="App">
+            <Header/>
+            <div className='conteudo'>
+                    <img src={background} className='background' alt="background-image" />
+                    <div className='info'>
+                        <div>
+                            <input name='usuario' placeholder='@username' />
+                            <button>Buscar</button>
+                        </div>
+                    </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default App;
