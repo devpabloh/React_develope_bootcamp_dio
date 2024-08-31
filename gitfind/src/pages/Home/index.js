a7918e650db80fc98d9b586a353131fefafa80ec
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-31 11:57:29 -0300

    Update index.js

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {Header} from '../../components/Header'
 import ItemList from '../../components/ItemList'
 import background from "../../assets/image-background.svg"
@@ -5,6 +6,10 @@ import './styles.css'
 
 
 function App(){
+    const [user, setUser] = useState('');
+    const [currentUser, setCurrentUser] = useState(null);
+
+
     return (
         <div className="App">
             <Header/>
@@ -12,11 +17,11 @@ function App(){
                     <img src={background} className='background' alt="background-image" />
                     <div className='info'>
                         <div>
-                            <input name='usuario' placeholder='@username' />
+                            <input name='usuario' value={user} onChange={event => setUser(event.target.value)} placeholder='@username' />
                             <button>Buscar</button>
                         </div>
                         <div className='perfil'>
-                            <img src="" className="profile" alt="imagem de perfil no github" />
+                            <img src="https://avatars.githubusercontent.com/u/146978584?v=4" className="profile" alt="imagem de perfil no github" />
                            <div>
                            <h3>Pablo Henrique</h3>
                            <span>@Dev. Pablo H.</span>
